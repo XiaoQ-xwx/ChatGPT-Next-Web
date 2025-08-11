e0619d70bf40accec7506b9e3eab9d48afd718e8
XiaoQ <96773038+XiaoQ-xwx@users.noreply.github.com>
2025-08-12 00:24:39 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -55,10 +55,10 @@ export const CN_MASKS: BuiltinMask[] = [
       sendMemory: true,
       historyMessageCount: 12,
       compressMessageLengthThreshold: 1000,
+      hideContext: true,   
     },
     lang: "cn",
     builtin: true,
-    hideContext: true,
     createdAt: 1754923999759,
   },
   {
